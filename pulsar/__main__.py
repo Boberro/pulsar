e54c5990f23d3972e2f0389c57ed398e1b522bd9
boberro <m.cyraniak@gmail.com>
2018-02-22 00:02:19 +0100

    working logging

--- pulsar/__main__.py
+++ pulsar/__main__.py
@@ -5,21 +5,51 @@ import csv
 import logging
 from pulsar import Pulsar
 
+logger = logging.getLogger('pulsar')
+logger_output = logging.getLogger('pulsar_output')
 
-def setup_logger(path, level):
-    if level.lower() == 'debug':
-        level = logging.DEBUG
-    elif level.lower() == 'info':
-        level = logging.INFO
-    elif level.lower() == 'warn':
-        level = logging.WARN
-    else:
-        print 'Warning: Incorrect log level set in configuration file. Using WARN level instead.'
-        level = logging.WARN
 
-    logger = logging.getLogger('pulsar')
-    logger.setLevel(level)
-    logging.basicConfig(filename=path, level=level)
+def setup_loggers(
+        file_enabled, console_enabled, file_path, logger_level,
+        output_file_enabled, output_console_enabled, output_path):
+    def get_level(level):
+        if level.lower() == 'debug':
+            return logging.DEBUG
+        elif level.lower() == 'info':
+            return logging.INFO
+        elif level.lower() == 'warn':
+            return logging.WARN
+        elif level.lower() == 'error':
+            return logging.ERROR
+        else:
+            print 'Warning: Incorrect log level set in configuration file. Using ERROR level instead.'
+            return logging.ERROR
+
+    # region Status Logger
+    logger.setLevel(get_level(logger_level))
+
+    if file_enabled.lower() == 'true':
+        file_handler = logging.FileHandler(file_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        logger.addHandler(file_handler)
+    if console_enabled.lower() == 'true':
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        logger.addHandler(stream_handler)
+    # endregion
+
+    # region Output Logger
+    logger_output.setLevel(logging.INFO)
+
+    if output_file_enabled.lower() == 'true':
+        output_file_handler = logging.FileHandler(output_path)
+        output_file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        logger_output.addHandler(output_file_handler)
+    if output_console_enabled.lower() == 'true':
+        output_stream_handler = logging.StreamHandler()
+        output_stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        logger_output.addHandler(output_stream_handler)
+    # endregion
 
 
 if __name__ == '__main__':
@@ -35,25 +65,31 @@ if __name__ == '__main__':
     try:
         refresh_time = config_parser.get('main', 'refresh_time')
         url_list_path = config_parser.get('main', 'url_list_path')
-        log_path = config_parser.get('logging', 'log_path')
-        log_level = config_parser.get('logging', 'log_level')
+        output_to_file = config_parser.get('output logger', 'log_to_file')
+        output_to_console = config_parser.get('output logger', 'log_to_console')
+        output_file_path = config_parser.get('output logger', 'path')
+        log_to_file = config_parser.get('status logger', 'log_to_file')
+        log_to_console = config_parser.get('status logger', 'log_to_console')
+        logger_file_path = config_parser.get('status logger', 'path')
+        log_level = config_parser.get('status logger', 'level')
     except ConfigParser.NoOptionError as e:
         print 'Error: Config file error: {}'.format(e)
     except ConfigParser.NoSectionError as e:
         print 'Error: Config file error: {}. Is {} correct path?'.format(e, config_path)
     else:
-        setup_logger(log_path, log_level)
+        setup_loggers(
+            log_to_file, log_to_console, logger_file_path, log_level,
+            output_to_file, output_to_console, output_file_path
+        )
 
         try:
             with open(url_list_path, 'rb') as url_list_file:
                 url_list_reader = csv.reader(url_list_file, delimiter=';', quotechar="\"")
                 url_list = [row for row in url_list_reader]
         except IOError as e:
-            logging.warning('Error: Could not find url definition file {}'.format(url_list_path))
+            logger.warning('Error: Could not find url definition file {}'.format(url_list_path))
         else:
             pulsar = Pulsar(refresh_time, url_list)
             pulsar.start()
 
-    print "Pulsar closed."
-
-    # print _requests('http://www.rarlab.com/rar/winrar-x64-420.exe')
+    logger.info("Pulsar closed.")
--- pulsar/pulsar.py
+++ pulsar/pulsar.py
@@ -2,7 +2,11 @@
 import requests
 from requests.exceptions import ConnectionError
 import time
+import logging
 
+
+logger = logging.getLogger('pulsar')
+logger_output = logging.getLogger('pulsar_output')
 
 class Pulsar(object):
     def __init__(self, refresh_time, url_list):
@@ -15,11 +19,12 @@ class Pulsar(object):
             r = requests.get(url)
             t2 = time.time()
         except ConnectionError as e:
-            print 'Connection error: {}'.format(url)
+            logger_output.warning('{} connection error'.format(url))
         else:
             total_time = t2 - t1
-            print url, total_time
+            logger_output.info('{} total time: {}s'.format(url, total_time))
 
     def start(self):
+        logger.info('Starting pulsar')
         for url, text_to_find in self.url_list:
             self.check_url(url, text_to_find)
