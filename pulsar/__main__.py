bc0f5a27df3d1f8e64b4d66dbbaa8b92d1d12649
boberro <m.cyraniak@gmail.com>
2018-02-21 21:33:21 +0100

    working on logger

--- pulsar/__main__.py
+++ pulsar/__main__.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 import sys
-import requests
-import time
 import ConfigParser
 import csv
-from pulsar import run_pulsar
+import logging
+from pulsar import Pulsar
 
 
-def _requests(url):
-    t1 = time.time()
-    r = requests.get(url)
-    t2 = time.time()
-    return t2 - t1, r.elapsed
+def setup_logger(path, level):
+    if level.lower() == 'debug':
+        level = logging.DEBUG
+    elif level.lower() == 'info':
+        level = logging.INFO
+    elif level.lower() == 'warn':
+        level = logging.WARN
+    else:
+        print 'Warning: Incorrect log level set in configuration file. Using WARN level instead.'
+        level = logging.WARN
+
+    logger = logging.getLogger('pulsar')
+    logger.setLevel(level)
+    logging.basicConfig(filename=path, level=level)
 
 
 if __name__ == '__main__':
@@ -27,19 +35,24 @@ if __name__ == '__main__':
     try:
         refresh_time = config_parser.get('main', 'refresh_time')
         url_list_path = config_parser.get('main', 'url_list_path')
+        log_path = config_parser.get('logging', 'log_path')
+        log_level = config_parser.get('logging', 'log_level')
     except ConfigParser.NoOptionError as e:
         print 'Error: Config file error: {}'.format(e)
     except ConfigParser.NoSectionError as e:
         print 'Error: Config file error: {}. Is {} correct path?'.format(e, config_path)
     else:
+        setup_logger(log_path, log_level)
+
         try:
             with open(url_list_path, 'rb') as url_list_file:
                 url_list_reader = csv.reader(url_list_file, delimiter=';', quotechar="\"")
                 url_list = [row for row in url_list_reader]
         except IOError as e:
-            print 'Error: Could not find url definition file {}'.format(url_list_path)
+            logging.warning('Error: Could not find url definition file {}'.format(url_list_path))
         else:
-            run_pulsar(refresh_time, url_list)
+            pulsar = Pulsar(refresh_time, url_list)
+            pulsar.start()
 
     print "Pulsar closed."
 
--- pulsar/pulsar.py
+++ pulsar/pulsar.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
+import requests
+from requests.exceptions import ConnectionError
+import time
+
+
 class Pulsar(object):
     def __init__(self, refresh_time, url_list):
         self.refresh_time = refresh_time
         self.url_list = url_list
 
+    def check_url(self, url, text_to_find):
+        try:
+            t1 = time.time()
+            r = requests.get(url)
+            t2 = time.time()
+        except ConnectionError as e:
+            print 'Connection error: {}'.format(url)
+        else:
+            total_time = t2 - t1
+            print url, total_time
+
     def start(self):
-        print "start", self.refresh_time, self.url_list
+        for url, text_to_find in self.url_list:
+            self.check_url(url, text_to_find)
